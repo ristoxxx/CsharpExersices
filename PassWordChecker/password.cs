9999f52f651ac72a474429b6bdbc51441e35ac70
ristoxxx <lahteri@gmail.com>
2024-02-25 21:30:02 +0200

    Create password.cs

---
+++ PassWordChecker/password.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int minLength = 8;
+      int score = 0;
+      string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowercase = "abcdefghijklmnopqrstuvwxyz";
+      string digits = "1234567890";
+      string specialChars = "!#¤%&/()=?`´+-*/";
+      Console.WriteLine("Enter password: ");
+      string password = Console.ReadLine();
+      if (password.Length >= minLength) {
+        score++;
+      } 
+      if (Tools.Contains(password,uppercase)) {
+        score++;
+      }
+      if (Tools.Contains(password,lowercase)) {
+        score++;
+      }
+      if (Tools.Contains(password,digits)) {
+        score++;
+      }
+      if (Tools.Contains(password,specialChars)) {
+        score++;
+      }
+      switch(score) {
+        case 1:
+        Console.WriteLine("Weak");
+        break;
+        case 2:
+        Console.WriteLine("Medium");
+        break;
+        case 3:
+        Console.WriteLine("Strong");
+        break;
+        case 4:
+        case 5:
+        Console.WriteLine("Extremely strong");
+        break;
+        default:
+        Console.WriteLine("None");
+        break;
+      }
+
+
+      //Console.WriteLine(score);
+    }
+  }
+}

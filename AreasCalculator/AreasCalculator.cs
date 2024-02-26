d19312c2c06edbcfce166fa1dfe9cdea4b0f9f27
ristoxxx <lahteri@gmail.com>
2024-02-27 00:07:41 +0200

    Create AreasCalculator.cs

---
+++ AreasCalculator/AreasCalculator.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ArchitectArithmetic
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      double teotichuan = (rentacleArea(1500,2500))+(0.5*circleArea(375))+(triangleArea(500,750));
+      double price = Math.Floor(teotichuan * 180);
+      Console.WriteLine($"price: {price}");
+    }
+    public static double rentacleArea(double length, double width) {
+      return (length*width);
+    }
+    public static double circleArea(double radius) {
+      return (Math.PI * Math.Pow(radius,2));
+    }
+    public static double triangleArea(double height, double bottom) {
+      return (0.5*bottom*height); 
+    }
+
+  }
+}

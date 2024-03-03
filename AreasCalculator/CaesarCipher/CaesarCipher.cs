79639a3e6f61e3f406b6f6f757081c3d6bc58ba1
ristoxxx <lahteri@gmail.com>
2024-03-03 16:57:29 +0200

    converts alphabet only

--- AreasCalculator/CaesarCipher/CaesarCipher.cs
+++ AreasCalculator/CaesarCipher/CaesarCipher.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace CaesarCipher
 {
@@ -9,18 +10,22 @@ namespace CaesarCipher
       char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
       string message = "";
       Console.WriteLine("Enter message");
-      message = Console.ReadLine();
+      message = Console.ReadLine().ToLower();
       char[] secretMessage = message.ToCharArray();
       char[] encryptedMessage = new char[secretMessage.Length];
       for (int i = 0; i < secretMessage.Length; i++) {
         char letter = secretMessage[i];
+         string pattern = "abcdefghijklmnopqrstuvwxyz";
+        if (!pattern.Contains(letter)) {
+          encryptedMessage[i] = letter;
+        } else {
         int index = Array.IndexOf(alphabet, letter);
-        index = index + 3;
+        index = (index + 3) % 26;
         char newLetter = alphabet[index];
         encryptedMessage[i] = newLetter;
-
         }
-      string newMessage = new string(string.Join(encryptedMessage));
+        }
+      string newMessage = String.Join("", encryptedMessage);
       Console.WriteLine("Encrypted message: " + newMessage);
       }
     }

a126d1b0c500fc65dea77fdb6e42258d1b668cdc
ristoxxx <lahteri@gmail.com>
2024-03-03 17:20:33 +0200

    refactored exported Encrypt as method

--- AreasCalculator/CaesarCipher/CaesarCipher.cs
+++ AreasCalculator/CaesarCipher/CaesarCipher.cs
@@ -7,26 +7,32 @@ namespace CaesarCipher
   {
     static void Main(string[] args)
     {
-      char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+      
       string message = "";
       Console.WriteLine("Enter message");
       message = Console.ReadLine().ToLower();
       char[] secretMessage = message.ToCharArray();
-      char[] encryptedMessage = new char[secretMessage.Length];
-      for (int i = 0; i < secretMessage.Length; i++) {
-        char letter = secretMessage[i];
-         string pattern = "abcdefghijklmnopqrstuvwxyz";
-        if (!pattern.Contains(letter)) {
-          encryptedMessage[i] = letter;
-        } else {
-        int index = Array.IndexOf(alphabet, letter);
-        index = (index + 3) % 26;
-        char newLetter = alphabet[index];
-        encryptedMessage[i] = newLetter;
-        }
-        }
-      string newMessage = String.Join("", encryptedMessage);
+      string newMessage = String.Join("", (Encrypt(secretMessage, 3)));
       Console.WriteLine("Encrypted message: " + newMessage);
-      }
     }
+    static char[] Encrypt(char[] secretMessage, int key) {
+        char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        char[] encryptedMessage = new char[secretMessage.Length];
+        for (int i = 0; i < secretMessage.Length; i++) {
+            char letter = secretMessage[i];
+            string pattern = "abcdefghijklmnopqrstuvwxyz";
+            if (!pattern.Contains(letter)) {
+                encryptedMessage[i] = letter;
+            } else {
+                int index = Array.IndexOf(alphabet, letter);
+                index = (index + key) % 26;
+                char newLetter = alphabet[index];
+                encryptedMessage[i] = newLetter;
+            }
+            }
+        return encryptedMessage;
+    }
+
   }
+}
+

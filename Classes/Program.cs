acb2bcae1ad5bd48486e7d74bf2b8f6b5fea8c81
ristoxxx <lahteri@gmail.com>
2024-03-05 11:24:49 +0200

    Update Program.cs

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,18 +1,12 @@
 using System;
 
-namespace BasicClasses
+namespace StaticMembers
 {
   class Program
   {
     static void Main(string[] args)
     {
-      Forest f = new Forest();
-      f.name = "Congo";
-      f.trees = 0;
-      f.age = 0;
-      f.biome = "Tropical";
-      
-      Console.WriteLine(f.name);
+      Forest.PrintTreeFacts();
     }
   }
 }

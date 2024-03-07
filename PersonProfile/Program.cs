05fa2d881f78ea14ef29cc7647a3bd44f6ee4b37
ristoxxx <lahteri@gmail.com>
2024-03-07 20:57:43 +0200

    Update Program.cs

--- PersonProfile/Program.cs
+++ PersonProfile/Program.cs
@@ -1,13 +1,39 @@
 using System;
 
 namespace DatingProfile
-{
-  class Program
+{ 
+  class Profile
+  { 
+  private string name;
+  private int age;
+  private string city;
+  private string country;
+  private string pronouns;
+  private string[] hobbies;
+  
+
+  public Profile(string name, int age, string city, string country, string pronouns = "they/them")
   {
-    static void Main(string[] args)
+    this.name = name;
+    this.age = age;
+    this.city = city;
+    this.country = country;
+    this.pronouns = pronouns;
+    this.hobbies = new string[0];
+  }
+  public string ViewProfile()
+  {
+    string hobbyList ="";
+    foreach (string hobby in hobbies)
     {
-      Profile sam = new Profile();
-      sam.name = "Sam Drakkila";
-    }
+      hobbyList += $"{hobby},\n\t";
+    } 
+    string test = ($"Name: {name},\nAge: {age},\nCity: {city},\nCountry: {country},\nPronouns: {pronouns},\nHobbies: {hobbyList}");
+    return test;
+  }
+  public void SetHobbies(string[] hobbies)
+  {
+    this.hobbies = hobbies;
+  }
   }
 }

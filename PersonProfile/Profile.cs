7d15a770dab16055ca61fb8facf848f08cb0f03c
ristoxxx <lahteri@gmail.com>
2024-03-07 20:57:55 +0200

    Update Profile.cs

--- PersonProfile/Profile.cs
+++ PersonProfile/Profile.cs
@@ -10,14 +10,30 @@ namespace DatingProfile
   private string country;
   private string pronouns;
   private string[] hobbies;
-  }
+  
 
-  public Profile(int age, string city, string country, string pronouns = "they/them", string[] hobbies)
+  public Profile(string name, int age, string city, string country, string pronouns = "they/them")
   {
+    this.name = name;
     this.age = age;
     this.city = city;
     this.country = country;
     this.pronouns = pronouns;
+    this.hobbies = new string[0];
+  }
+  public string ViewProfile()
+  {
+    string hobbyList ="";
+    foreach (string hobby in hobbies)
+    {
+      hobbyList += $"{hobby},\n\t";
+    } 
+    string test = ($"Name: {name},\nAge: {age},\nCity: {city},\nCountry: {country},\nPronouns: {pronouns},\nHobbies: {hobbyList}");
+    return test;
+  }
+  public void SetHobbies(string[] hobbies)
+  {
     this.hobbies = hobbies;
+  }
   }
 }

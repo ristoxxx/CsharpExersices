6acd099a5c93e62240916a0d7fa8a3a270e7f517
ristoxxx <lahteri@gmail.com>
2024-03-20 12:00:57 +0200

    Update first.cs

--- exerciseSgroup/first.cs
+++ exerciseSgroup/first.cs
@@ -1,1 +1,19 @@
+using System;
+
+namespace Pizza 
+{
+    class Slice
+    {
         Console.WriteLine("Hello World!")
+        switch (input)
+
+        {
+            case "UT":
+            taxRate =  6,85;
+            break;
+            case "NV":
+            default:
+        }
+    }
+
+}
